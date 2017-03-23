f8b8ad453717a450a22a67693c6b67f41f9469d1
Álvaro <ganzux@users.noreply.github.com>
2017-03-23 16:38:22 +0000

    Update Delta.java
    Using SYSTEM.IN correctly

--- src/com/ganzux/booking/Delta.java
+++ src/com/ganzux/booking/Delta.java
@@ -1,33 +1,38 @@
 package com.ganzux.booking;
 
+import java.util.Scanner;
+
 /*
  * Problem
-
 Given a list of numbers, e.g.:
-
 25626 25757 24367 24267 16 100 2 7277
 Output a delta encoding for the sequence. In a delta encoding, the first element is reproduced as is.
 Each subsequent element is represented as the numeric difference from the element before it. E.g.
 for the sequence above, the delta encoding would be:
-
 25626 131 -1390 -100 -24251 84 -98 7275
 However, if a difference value does not fit in a single signed byte, i.e. -127 <= x <= 127, then,
 instead of the difference, we would like to use an escape token, printing it.
-
 This will denote that the value following the escape token is a full four-byte difference value,
 rather than a one-byte different value.
-
 For this exercise, we'll declare -128 as the escape token.
-
 Following the same example above, the final result would be:
-
 25626 -128 131 -128 -1390 -100 -128 -24251 84 -98 -128 7275
  */
 
 public class Delta {
+	
+public static void main(String args[] ) throws Exception {
+		
+		Scanner scanner = new Scanner(System.in);
+		String myString = scanner.nextLine();
+		scanner.close();
 
-	public static void main(String args[] ) throws Exception {
-		String input = "25626 25757 24367 24267 16 100 2 7277";
+		delta(myString);
+	}
+
+
+	public static void delta(String input) throws Exception {
+
 		StringBuilder sb = new StringBuilder();
 		
 		Integer lastNumber = null;
@@ -52,7 +57,7 @@ public class Delta {
 			lastNumber = thisNumber;
 		}
 		
-		System.out.println(sb.toString());
+		System.out.println(sb.toString().substring(0, sb.toString().length() - 1));
 		
 	}
 }
